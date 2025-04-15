5a6d2922691431565bd711644b7998350cdd6138
PASCAL Noa <npascal@gaming.tech>
2025-04-16 01:42:38 +0200

    Revert "Merge branch 'Noa'"
    This reverts commit da238c63f6aaf8d337556999d1e1c7d0e09682a1, reversing
    changes made to fc8e0d571dc84bf0358779c6d161ce5f3d9e2373.

--- Geometry.py
+++ Geometry.py
@@ -47,76 +47,14 @@ class Geometry:
         return Geometry.transpose(W)
 
     @staticmethod
-    def trapeze_plein(n, base_large, base_etroite, hauteur, epaisseur):
-        W = []
-        n2 = int(n ** (1 / 3))
-        for i in range(n2):
-            for j in range(n2):
-                for k in range(n2):
-                    ratio = i / n2  
-                    largeur = base_large * (1 - ratio) + base_etroite * ratio
-                    y = -largeur / 2 + largeur * j / n2
-                    z = -epaisseur / 2 + epaisseur * k / n2
-                    x = -hauteur / 2 + hauteur * i / n2
-                    W.append([x, y, z])
-        return Geometry.transpose(W)
-
-    @staticmethod
-    def trapeze_volume(n, p1, p2, p3, p4, epaisseur):
-        def vec_add(a, b): return [a[i] + b[i] for i in range(3)]
-        def vec_sub(a, b): return [a[i] - b[i] for i in range(3)]
-        def vec_scale(a, s): return [a[i] * s for i in range(3)]
-        def vec_dot(a, b): return sum(a[i] * b[i] for i in range(3))
-        def vec_cross(a, b):
-            return [
-                a[1]*b[2] - a[2]*b[1],
-                a[2]*b[0] - a[0]*b[2],
-                a[0]*b[1] - a[1]*b[0]
-            ]
-        def vec_norm(v): return sum(x**2 for x in v) ** 0.5
-        def vec_normalize(v):
-            norm = vec_norm(v)
-            return [x / norm for x in v] if norm != 0 else [0, 0, 0]
-
-        n2 = int(n ** (1/3))
-        W = []
-
-        for i in range(n2):
-            for j in range(n2):
-                t1 = j / (n2 - 1)
-                t2 = i / (n2 - 1)
-
-                A = vec_add(vec_scale(p1, 1 - t1), vec_scale(p2, t1))
-                B = vec_add(vec_scale(p4, 1 - t1), vec_scale(p3, t1))
-                point_surface = vec_add(vec_scale(A, 1 - t2), vec_scale(B, t2))
-
-                normal = vec_cross(vec_sub(p2, p1), vec_sub(p4, p1))
-                normal = vec_normalize(normal)
-
-                for k in range(n2):
-                    depth = -epaisseur / 2 + epaisseur * k / n2
-                    P = vec_add(point_surface, vec_scale(normal, depth))
-                    W.append(P)
-
-        return Geometry.transpose(W)
-
-
-    @staticmethod
     def solide(n):
         # Proportions des différentes parties
         n_fuselage = int(n * 0.4)
         n_ailes = int(n * 0.3)
         n_queue = int(n * 0.3)
 
-        # Fuselage central : cylindre long et fin, initialement orienté sur Z
+        # Fuselage central : cylindre long et fin
         fuselage = Geometry.cylindre(n_fuselage, R=0.3, h=4)
-
-        # Rotation du fuselage pour l’orienter sur l’axe X
-        for i in range(len(fuselage[0])):
-            v = Vector([fuselage[0][i], fuselage[1][i], fuselage[2][i]])
-            v_rotated = v.rotate('y', -pi / 2)
-            fuselage[0][i], fuselage[1][i], fuselage[2][i] = v_rotated.coords
-
 
         # Ailes : pavés plats, décalés sur l'axe Y
         ailes_gauche = Geometry.pave_plein(n_ailes // 2, a=2, b=0.1, c=0.02)
@@ -142,3 +80,27 @@ class Geometry:
         Z = fuselage[2] + ailes_gauche[2] + ailes_droite[2] + queue[2]
 
         return [X, Y, Z]
+
+    def center_of_mass(self, points, masses):
+        if len(points) != len(masses):
+            raise ValueError("Le nombre de points doit être égal au nombre de masses")
+
+        total_mass = sum(masses)
+        if total_mass == 0:
+            raise ValueError("La masse totale ne peut pas être nulle")
+
+        weighted_sum = [0, 0, 0]
+        for p, m in zip(points, masses):
+            weighted_sum = [w + m * x for w, x in zip(weighted_sum, p.coords)]
+
+        return Vector([x / total_mass for x in weighted_sum])
+
+    def moment(self, F, r):
+        return r.cross(F)
+
+    def triple_scalar(self, a, b, c):
+        return a.dot(b.cross(c))
+
+    def is_colinear(self, v1, v2):
+        cross_prod = v1.cross(v2)
+        return cross_prod.norm() < 1e-6
