1ade9d62097ded40de0d866588a56041af42546f
PASCAL Noa <npascal@gaming.tech>
2025-04-16 01:06:15 +0200

    [REF] Code review

--- Geometry.py
+++ Geometry.py
@@ -87,27 +87,3 @@ class Geometry:
         Z = fuselage[2] + ailes_gauche[2] + ailes_droite[2] + queue[2]
 
         return [X, Y, Z]
-
-    def center_of_mass(self, points, masses):
-        if len(points) != len(masses):
-            raise ValueError("Le nombre de points doit être égal au nombre de masses")
-
-        total_mass = sum(masses)
-        if total_mass == 0:
-            raise ValueError("La masse totale ne peut pas être nulle")
-
-        weighted_sum = [0, 0, 0]
-        for p, m in zip(points, masses):
-            weighted_sum = [w + m * x for w, x in zip(weighted_sum, p.coords)]
-
-        return Vector([x / total_mass for x in weighted_sum])
-
-    def moment(self, F, r):
-        return r.cross(F)
-
-    def triple_scalar(self, a, b, c):
-        return a.dot(b.cross(c))
-
-    def is_colinear(self, v1, v2):
-        cross_prod = v1.cross(v2)
-        return cross_prod.norm() < 1e-6
